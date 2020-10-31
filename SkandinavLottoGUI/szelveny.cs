9af97a4bd28f2915876c308b049afa57e180c26f
Bornemissz Zsigmond <bornemissza.zsigmond@ganziskola.hu>
2020-10-31 16:00:20 +0100

    szelveny class szamokSzama() szamokDb()

---
+++ SkandinavLottoGUI/szelveny.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SkandinavLottoGUI
+{
+    class szelveny
+    {
+        public List<string> megjatszottSzamok { get; set; }
+
+        public szelveny(TextBox textBoxString)
+        {
+            megjatszottSzamok = textBoxString.Text.Split(' ').ToList();
+            if (megjatszottSzamok.Last().Equals("")) megjatszottSzamok.RemoveAt(megjatszottSzamok.Count - 1);
+        }
+
+        public bool szamokSzama()
+        {
+            if (megjatszottSzamok.Count != 7) return  true;
+            else return false;
+        }
+
+        public int szamokDb() { return megjatszottSzamok.Count; }
+    }
+}
