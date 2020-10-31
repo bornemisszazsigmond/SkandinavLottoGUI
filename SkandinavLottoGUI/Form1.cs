595ac66b6effe8ecf29b00ac75ec4ad9f317e676
Bornemissz Zsigmond <bornemissza.zsigmond@ganziskola.hu>
2020-10-31 16:22:20 +0100

    Végleges verzió - tesztelve

--- SkandinavLottoGUI/Form1.cs
+++ SkandinavLottoGUI/Form1.cs
@@ -12,8 +12,9 @@ namespace SkandinavLottoGUI
 {
     public partial class Form1 : Form
     {
-        List<string> szamok = new List<string>();
+        szelveny aktualis = new szelveny();
         Random vel = new Random();
+        
         public Form1()
         {
             InitializeComponent();
@@ -22,69 +23,44 @@ namespace SkandinavLottoGUI
         private void textBoxSzamok_TextChanged(object sender, EventArgs e)
         {
 
-            szelveny aktualis = new szelveny(textBoxSzamok);
-            
+            aktualis = new szelveny(textBoxSzamok);
+
             //7 db szám ellenőrzése
-            if(aktualis.szamokSzama()) checkBoxTippekSzama.Checked = true;
+            if (aktualis.szamokSzama()) checkBoxTippekSzama.Checked = true;
             else checkBoxTippekSzama.Checked = false;
             labelSzamDb.Text = aktualis.szamokDb().ToString();
 
-
-            //szamok = textBoxSzamok.Text.Split(' ').ToList();
-            //if (szamok.Last().Equals("")) szamok.RemoveAt(szamok.Count-1);
-
-            /*dbSzamok();
-            dbJoSzam();
-            if (!checkBoxNemMegfeleloSzamok.Checked) azonosSzamok();
-
-            if (!checkBoxTippekSzama.Checked && !checkBoxNemMegfeleloSzamok.Checked && !checkBoxAzonosSzamok.Checked) buttonJatek.Enabled = true;
-            else buttonJatek.Enabled = false;*/
-        }
-
-        /*private void azonosSzamok()
-        {
-            HashSet<int> ellenorzes = new HashSet<int>();
-            foreach (var item in szamok) ellenorzes.Add(Convert.ToInt32(item));
-            if (ellenorzes.Count != szamok.Count) checkBoxAzonosSzamok.Checked = true;
-            else checkBoxAzonosSzamok.Checked = false;
-        }*/
-
-        /*private void dbJoSzam()
-        {          
-            int db = 0;
-            foreach (var item in szamok)
-            {
-                try
-                {
-                    if (Convert.ToInt32(item) < 1 || Convert.ToInt32(item) > 35) db++;
-                }
-                catch (Exception)
-                {
-                    db++;
-                }              
-            }
-            if(db>0) checkBoxNemMegfeleloSzamok.Checked = true;
+            //Nem jó tippek száma
+            if (aktualis.nemMegfeleltSzamok() > 0) checkBoxNemMegfeleloSzamok.Checked = true;
             else checkBoxNemMegfeleloSzamok.Checked = false;
-            labelNemJoSzam.Text = db.ToString();
+            labelNemJoSzam.Text = aktualis.nemMegfeleltSzamok().ToString();
+
+            //Azonos számok
+            if (aktualis.azonosSzamok()) checkBoxAzonosSzamok.Checked = true;
+            else checkBoxAzonosSzamok.Checked = false;
+            
+            //Tippek beküldése
+            if (!checkBoxTippekSzama.Checked && !checkBoxNemMegfeleloSzamok.Checked && !checkBoxAzonosSzamok.Checked) buttonJatek.Enabled = true;
+            else buttonJatek.Enabled = false;
         }
-        */
-        
-        /*public void sorsoltSzamok(TextBox nev, Label db)
+
+        private void buttonJatek_Click(object sender, EventArgs e)
+        {
+            sorsoltSzamok(textBoxGepi, labelGepiTalaltDb);
+            sorsoltSzamok(textBoxKezi, labelKeziTalaltDb);
+        }
+
+
+        public void sorsoltSzamok(TextBox nev, Label db)
         {
             SortedSet<int> sorsolas = new SortedSet<int>();
             while (sorsolas.Count != 7) sorsolas.Add(vel.Next(1, 36));
             nev.Text = string.Join(" ",sorsolas);
 
             HashSet<int> metszet = new HashSet<int>();
-            foreach (var item in szamok) metszet.Add(Convert.ToInt32(item));
+            foreach (var item in aktualis.megjatszottSzamok) metszet.Add(Convert.ToInt32(item));
             metszet.IntersectWith(sorsolas);
             db.Text = metszet.Count.ToString();
-        }*/
-        
-        private void buttonJatek_Click(object sender, EventArgs e)
-        {
-            //sorsoltSzamok(textBoxGepi, labelGepiTalaltDb);
-            //sorsoltSzamok(textBoxKezi, labelKeziTalaltDb);
         }
     }
 }
--- SkandinavLottoGUI/szelveny.cs
+++ SkandinavLottoGUI/szelveny.cs
@@ -17,6 +17,11 @@ namespace SkandinavLottoGUI
             if (megjatszottSzamok.Last().Equals("")) megjatszottSzamok.RemoveAt(megjatszottSzamok.Count - 1);
         }
 
+        public szelveny()
+        {
+
+        }
+
         public bool szamokSzama()
         {
             if (megjatszottSzamok.Count != 7) return  true;
@@ -24,5 +29,30 @@ namespace SkandinavLottoGUI
         }
 
         public int szamokDb() { return megjatszottSzamok.Count; }
+
+        public int nemMegfeleltSzamok()
+        {
+            int db = 0;
+            foreach (var item in megjatszottSzamok)
+            {
+                try
+                {
+                    if (Convert.ToInt32(item) < 1 || Convert.ToInt32(item) > 35) db++;
+                }
+                catch (Exception)
+                {
+                    db++;
+                }
+            }
+            return db;
+        }
+
+        public bool azonosSzamok()
+        {
+            HashSet<int> ellenorzes = new HashSet<int>();
+            foreach (var item in megjatszottSzamok) ellenorzes.Add(Convert.ToInt32(item));
+            if (ellenorzes.Count != megjatszottSzamok.Count) return true;
+            else return false;
+        }
     }
 }
